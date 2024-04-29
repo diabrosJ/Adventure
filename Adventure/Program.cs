ead30295d9f5e3f901b77d3ce8891c378aa2b77e
정창영 <bfcat46@gmail.com>
2024-04-29 20:03:29 +0900

    Update Program.cs

--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Adventure
+﻿using System;
+
+namespace Adventure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
+            Console.WriteLine("정창영");
             Console.WriteLine("정창영");
         }
     }
