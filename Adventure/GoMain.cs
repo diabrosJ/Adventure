9439bbd903c3c8d3c6573990c35b53032868c4da
diabrosJ <167047057+diabrosJ@users.noreply.github.com>
2024-04-30 10:25:14 +0900

    init
    스크립트 파일 4개 추가 및 데이터 내용 추가 Gomain 주석 확인 부탁드립니다.

---
+++ Adventure/GoMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure
+{
+    internal class GoMain
+    {
+        public void mainScence()
+        {
+            Console.WriteLine(" 스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine(" 전투 시작에 앞서 행동을 선택해 주세요. ");
+            Console.Write(Console.ReadLine());
+        }
+    }
+}
--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("안지수");
+
         }
     }
 }
