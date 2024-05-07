28f622dd2895315586aabe6f85e333f96c380c6f
201870024YuSeungHyeon <jyyootom@naver.com>
2024-05-07 11:51:25 +0900

    최종

--- Adventure/Charter/CreateFactory/Monster.cs
+++ Adventure/Charter/CreateFactory/Monster.cs
@@ -11,9 +11,9 @@ namespace Adventure.Charter.CreateFactory
 
         public static Dictionary<string, CreateMonster> Monsters = new Dictionary<string, CreateMonster>
         {
-            {"CannonMinion", new CreateMonster("대포미니언", 2, 30, 10)},
-            {"MeleeMinion", new CreateMonster("미니언", 1, 20, 5)},
-            {"Voidling", new CreateMonster("공허충", 1, 50, 3)}
+            {"CannonMinion", new CreateMonster("대포미니언", 2, 30, 12)},
+            {"MeleeMinion", new CreateMonster("미니언", 1, 20, 8)},
+            {"Voidling", new CreateMonster("공허충", 1, 50, 5)}
         };
         Monster()
         {
